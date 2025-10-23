13e445aba3e1489a0ef402154d6db887c3bcf4c9
Davidshtp <davidtrujillo2207@gmail.com>
2025-10-23 02:07:26 -0500

    Desarrollo de Vistas Auth Header y SideBar

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Error404 from "./pages/Error404";
+import Register from "./pages/auth/Register";
+import LayoutAdmin from "./layouts/LayoutAdmin";
+import Home from "./pages/admin/Home";
+import LayoutAuth from "./layouts/LayoutAuth";
+import ForgetPassword from "./pages/auth/ForgetPassword";
+
+function App() {
+  return <BrowserRouter>
+    <Routes>
+
+      <Route path="/auth" element={<LayoutAuth />}>
+        <Route index element={<Login />} />
+        <Route path="registro" element={<Register />} />
+        <Route path="olvide-password" element={<ForgetPassword />} />
+      </Route>
+
+      <Route path="/" element={<LayoutAdmin />}>
+        <Route index element={<Home />} />
+      </Route>
+      //todas las demas rutas
+      <Route path="*" element={<Error404 />} />
+    </Routes>
+  </BrowserRouter>
+}
+
+export default App;
