6e0dfe7b06ff90c1736838f1830d57ee85821fbd
Morenosteven <stivenmoreno725@gmail.com>
2025-10-24 11:54:30 -0500

    Vista Perfil del Usurio

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -4,21 +4,34 @@ import Error404 from "./pages/Error404";
 import Register from "./pages/auth/Register";
 import LayoutAdmin from "./layouts/LayoutAdmin";
 import Home from "./pages/admin/Home";
+import Profile from "./pages/admin/Profile";
 import LayoutAuth from "./layouts/LayoutAuth";
 import ForgetPassword from "./pages/auth/ForgetPassword";
+<<<<<<< HEAD
+import { Profiler } from "react";
+=======
+>>>>>>> 75c6c821cd5a8f19ee5ee73a6573d4dbdb2ab1ea
 
 function App() {
   return <BrowserRouter>
     <Routes>
-
-      <Route path="/auth" element={<LayoutAuth />}>
+<<<<<<< HEAD
+      <Route path="/login" element={<LayoutAuth />}>
+      <Route path="registro" element={<Register />} />
+      <Route path="olvide-password" element={<ForgetPassword />} />
+      <Route path="/" element={<LayoutAdmin />}></Route>
         <Route index element={<Login />} />
-        <Route path="registro" element={<Register />} />
-        <Route path="olvide-password" element={<ForgetPassword />} />
       </Route>
-
+        <Route index element={<Home />} />
+        <Route path="/perfil" element={<Profile />}>
+=======
+      <Route path="/login" element={<Login />} />
+      <Route path="/registro" element={<Register />} />
+      <Route path="/olvide-password" element={<ForgetPassword />} />
       <Route path="/" element={<LayoutAdmin />}>
         <Route index element={<Home />} />
+        <Route path="perfil" element={<Profile />} />
+>>>>>>> 75c6c821cd5a8f19ee5ee73a6573d4dbdb2ab1ea
       </Route>
       //todas las demas rutas
       <Route path="*" element={<Error404 />} />
--- dashboard/src/pages/auth/Register.jsx
+++ dashboard/src/pages/auth/Register.jsx
@@ -7,7 +7,12 @@ const Register = () => {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
+<<<<<<< HEAD
     <div className="bg-secondary-100 p-8 rounded-xl shadow-2xl w-auto lg:w-[450px]">
+=======
+    <div className="min-h-screen flex items-center justify-center p-4">
+     <div className="bg-secondary-100 p-8 rounded-xl shadow-2xl w-auto lg:w-[450px]">
+>>>>>>> 75c6c821cd5a8f19ee5ee73a6573d4dbdb2ab1ea
       <h1 className="text-3xl  text-center uppercase font-bold tracking-[5px] text-white mb-8">Crear<span className="text-primary">Cuenta</span></h1>
       <form className="mb-8">
         <button className="flex items-center justify-center py-3 px-4 gap-4 bg-secondary-900 w-full rounded-full mb-8 text-gray-100">
@@ -82,12 +87,20 @@ const Register = () => {
       <span className="flex items-center justify-center gap-2">
         Ya tienes cuenta?
         <Link
+<<<<<<< HEAD
           to="/auth"
+=======
+          to="/login"
+>>>>>>> 75c6c821cd5a8f19ee5ee73a6573d4dbdb2ab1ea
           className="text-primary hover:text-gray-100 transition-colors">
           Ingresa
         </Link>
       </span>
 
+<<<<<<< HEAD
+=======
+     </div>
+>>>>>>> 75c6c821cd5a8f19ee5ee73a6573d4dbdb2ab1ea
     </div>
   );
 }
